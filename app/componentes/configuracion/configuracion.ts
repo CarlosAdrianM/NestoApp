70878954f4290cb99c3bd3c1638df4ecdd8c3ac6
Carlos Adrián Martínez <carlos_adrian_m@hotmail.com>
2016-07-05 23:49:09 +0200

    Puesto ionic deploy para que se actualice automaticamente la aplicacion

--- app/componentes/configuracion/configuracion.ts
+++ app/componentes/configuracion/configuracion.ts
@@ -1,6 +1,7 @@
 ﻿export class Configuracion {
     // public static URL_SERVIDOR: string = 'http://api.nuevavision.es';
-    public static URL_SERVIDOR: string = 'http://localhost:53364';
+    // public static URL_SERVIDOR: string = 'http://localhost:53364';
+    public static URL_SERVIDOR: string = 'http://88.26.231.83';
     public static API_URL: string = Configuracion.URL_SERVIDOR + '/api';
     public static EMPRESA_POR_DEFECTO: string = '1';
     public static ALMACEN_POR_DEFECTO: string = 'ALG';
