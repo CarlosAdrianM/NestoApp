57b0071ec832dba737ad653b6f6d6b5875748226
Carlos Adrián Martínez <carlos_adrian_m@hotmail.com>
2017-05-13 22:20:21 +0200

    Rapports corregido el error que rompia el menu al entrar a la lista de rapports

---
+++ src/pages/ListaRapports/ListaRapports.component.d.ts
@@ -0,0 +1,7 @@
+import { AlertController } from 'ionic-angular';
+export declare class ListaRapports {
+    private alertCtrl;
+    textoBoton: string;
+    constructor(alertCtrl: AlertController);
+    seleccionarContacto(): void;
+}
