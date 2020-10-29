f99aa72ed1431eb9101ed3ba740f16f46539ef1e
CarlosAdrianM <carlosadrian@nuevavision.es>
2020-10-29 10:25:56 +0100

    Versión 2.1.1

--- src/app/components/configuracion/configuracion/configuracion.component.ts
+++ src/app/components/configuracion/configuracion/configuracion.component.ts
@@ -16,7 +16,7 @@ export class Configuracion implements OnInit {
   public static NOMBRE_DOMINIO: string = 'NUEVAVISION';
   public static VENDEDOR_GENERAL: string = "NV";
 
-  public static VERSION: string = "2.1.0";
+  public static VERSION: string = "2.1.1";
 
   ngOnInit() {}
 
