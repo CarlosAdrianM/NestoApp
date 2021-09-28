a0c9f2faa97249804954be0c584142e7497a424f
CarlosAdrianM <carlosadrian@nuevavision.es>
2021-09-28 17:33:50 +0200

    Cambiada a rama master

---
+++ src/oauth.ts
@@ -0,0 +1,9 @@
+export const OAuthSettings = {
+    appId: '115eb7f5-3503-449b-bf89-c3eb9ae83038',
+    redirectUri: 'http://localhost:8100',
+    scopes: [
+      "user.read",
+      "mailboxsettings.read",
+      "calendars.readwrite"
+    ]
+  };
