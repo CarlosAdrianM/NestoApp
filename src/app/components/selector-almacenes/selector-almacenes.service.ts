165db14bcd272e60e846e0e9aec453c821d42970
CarlosAdrianM <carlosadrian@nuevavision.es>
2023-09-29 19:22:00 +0200

    Almacen ALC en plantillavta

--- src/app/components/selector-almacenes/selector-almacenes.service.ts
+++ src/app/components/selector-almacenes/selector-almacenes.service.ts
@@ -21,6 +21,10 @@ export class SelectorAlmacenesService {
           {
               almacen : "REI",
               descripcion : "Reina"
+          },
+          {
+              almacen : "ALC",
+              descripcion : "Alcobendas"
           }
       ];
       return of(listaAlmacenes);
