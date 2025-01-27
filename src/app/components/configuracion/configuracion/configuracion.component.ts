2656784e1d248114e2611b31e88bd23a1d0f0375
CarlosAdrianM <carlosadrian@nuevavision.es>
2025-01-27 19:54:31 +0100

    Puestos comentarios al crear clientes 2.6.3

--- src/app/components/configuracion/configuracion/configuracion.component.ts
+++ src/app/components/configuracion/configuracion/configuracion.component.ts
@@ -17,7 +17,7 @@ export class Configuracion implements OnInit {
   public static NOMBRE_DOMINIO: string = 'NUEVAVISION';
   public static VENDEDOR_GENERAL: string = "NV";
 
-  public static VERSION: string = "2.6.2";
+  public static VERSION: string = "2.6.3";
 
   ngOnInit() {}
 
